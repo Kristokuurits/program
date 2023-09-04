040c8c028badc638031306ba14f9bdf15b21a61d
Kristokuurits <127192906+Kristokuurits@users.noreply.github.com>
2023-09-04 12:02:35 +0300

    Add files via upload

---
+++ DbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using ÄRA_NÄPPI_MINU_FAILI.Models;
+
+namespace ÄRA_NÄPPI_MINU_FAILI.Data
+{
+    public class DbInitializer
+    {
+        public static void Initalize(SchoolContext context)
+        {
+            //otsib õpilasi
+            if (context.Students.Any())
+            {
+                return;//väljub meetodist kui andmebaas sisaldab juba andmeid ning meedotis kirjeltatuid näidisõpilasi//kursuseid ja aineosalusi ei lisata      
+            }
+        
+
+            var students = new Student[]
+            {
+                new Student {FirstMidName="Markus",LastName="Nirgi",EnrollmentDate=DateTime.Parse("2022-09-01")},
+                new Student {FirstMidName="Virkus",LastName="Uusküla",EnrollmentDate=DateTime.Parse("2022-09-01")},
+                new Student {FirstMidName="Juuli",LastName="Virtsu",EnrollmentDate=DateTime.Parse("2022-09-01")},
+                new Student {FirstMidName="Maali",LastName="Plätsu",EnrollmentDate=DateTime.Parse("2022-09-01")},
+                new Student {FirstMidName="Beta",LastName="Male",EnrollmentDate=DateTime.Parse("2022-09-01")},
+                new Student {FirstMidName="Alpha",LastName="Chad",EnrollmentDate=DateTime.Now}
+            };
+            foreach (Student s in students)
+            {
+                context.Students.Add(s);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using ÄRA_NÄPPI_MINU_FAILI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ÄRA_NÄPPI_MINU_FAILI.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> option) : base(options)
+        { 
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable(nameof(Course));
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
